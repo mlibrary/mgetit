2cf2f848f72c8ba83eb123f784e335487866d3ed
Earley <earleyj@m-d25q90jsfy13.local>
2016-08-04 16:17:36 -0400

    removed table css and improved list result

--- app/assets/javascripts/mgetit.js
+++ app/assets/javascripts/mgetit.js
@@ -11,25 +11,27 @@
     var show_more_sources = document.getElementById('show-more-sources');
     var digital_sources = document.getElementById('fulltext-sources').children;
 
-    for (var i = 0; i < digital_sources.length; i++) {
-      if (i > 0) {
-        addClass(digital_sources[i], 'hide')
-      }
-    }
-
-    removeClass(show_more_sources, 'hide')
-
-    show_more_sources.addEventListener('click', function(event) {
-      event.preventDefault();
-
-      addClass(show_more_sources, 'hide')
+    if (show_more_sources) { // only if there are more sources to show
 
       for (var i = 0; i < digital_sources.length; i++) {
-        removeClass(digital_sources[i], 'hide')
+        if (i > 0) {
+          addClass(digital_sources[i], 'hide')
+        }
       }
-    })
-  })
 
+      removeClass(show_more_sources, 'hide')
+
+      show_more_sources.addEventListener('click', function(event) {
+        event.preventDefault();
+
+        addClass(show_more_sources, 'hide')
+
+        for (var i = 0; i < digital_sources.length; i++) {
+          removeClass(digital_sources[i], 'hide')
+        }
+      })
+    }
+  })
 
   /*
     Helper Functions
