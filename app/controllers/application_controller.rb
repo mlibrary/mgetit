0acdb0d7a7bce3dcbe9cbf9f3d84ea7d11d22140
Albert Bertram <bertrama@umich.edu>
2016-10-16 23:02:41 -0400

    Added pre-deployment fixes.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,12 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  def redirect
+    if request.query_parameters.empty?
+      redirect_to '/citation-linker/', status: 302
+    else
+      redirect_to request.query_parameters.merge({ controller: 'resolve', action: 'index', status: 302})
+    end
+  end
 end
