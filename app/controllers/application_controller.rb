fbc9dc285051bd3c241ea59e68feeb0744090c7a
Albert Bertram <bertrama@umich.edu>
2016-10-26 18:41:56 -0400

    Bugfixes.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,8 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  skip_before_filter :verify_authenticity_token, only: [:redirect]
+
   def redirect
     if request.query_parameters.empty?
       redirect_to '/citation-linker/', status: 302
