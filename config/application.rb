a7847df1f8e3dda6ecbf811ea737ded0ca8577aa
Albert Bertram <bertrama@umich.edu>
2020-03-29 17:40:11 -0400

    Monkey patch for actionview.

--- config/application.rb
+++ config/application.rb
@@ -2,6 +2,7 @@ require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
 require File.expand_path('../../lib/mgetit/request_patch', __FILE__)
+require File.expand_path('../../lib/actionview_patch', __FILE__)
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
