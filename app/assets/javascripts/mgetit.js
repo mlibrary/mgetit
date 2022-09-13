ec2e6de8f6f59ce8310077c38b0af1f39dadd1ed
Erin E. Sullivan <sulliv.erin@gmail.com>
2022-09-13 09:37:10 -0400

    Removing collapsable option to show all items

--- app/assets/javascripts/mgetit.js
+++ app/assets/javascripts/mgetit.js
@@ -1,42 +1,5 @@
 (function() {
 
-  var manage_result_options = function() {
-    /*
-      Show More Sources/Links/Results
-    */
-    // Hide all but the first link on page load and then
-    // show the rest of the links when "show more" is clicked.
-
-    var show_more_sources = document.getElementById('show-more-sources');
-    var fulltext_sources = document.getElementById('fulltext-sources');
-    var digital_sources = [];
-    if (fulltext_sources) {
-      digital_sources = fulltext_sources.children;
-    }
-
-    if (show_more_sources) { // only if there are more sources to show
-
-      for (var i = 0; i < digital_sources.length; i++) {
-        if (i > 0) {
-          addClass(digital_sources[i], 'hide')
-        }
-      }
-
-      removeClass(show_more_sources, 'hide')
-
-      show_more_sources.addEventListener('click', function(event) {
-        event.preventDefault();
-
-        addClass(show_more_sources, 'hide')
-
-        for (var i = 0; i < digital_sources.length; i++) {
-          removeClass(digital_sources[i], 'hide')
-        }
-      })
-    }
-
-  }
-
   var dismiss = function() {
     var dismiss_elements = document.getElementsByClassName('dismiss');
 
@@ -69,7 +32,6 @@
   }
 
   document.addEventListener("DOMContentLoaded", function(event) {
-    manage_result_options()
     dismiss()
   })
 
