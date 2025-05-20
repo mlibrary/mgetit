af16a6b27c9acf5c1c14ad19c1422b3dfbd6b5f5
Albert Bertram <bertrama@umich.edu>
2025-05-20 13:09:40 -0400

    Bugfix in the string preprocessor.

--- lib/link_resolver/preprocess_query_string.rb
+++ lib/link_resolver/preprocess_query_string.rb
@@ -9,6 +9,7 @@ module LinkResolver
       doi = %r{https?%3[Aa]%2[Ff]%2[Ff](dx\.)?doi\.org%2[Ff]([^&]+)}
       pubmed = %r{https?%3[Aa]%2[Ff]%2[Ff]pubmed\.ncbi\.nlm\.nih\.gov%2[Ff](\d+)[^&]*}
       ["QUERY_STRING", "REQUEST_URI"].each do |key|
+        next if env[key].frozen?
         env[key]&.gsub!(doi, "\\2")
         env[key]&.gsub!(pubmed, "\\1")
       end
