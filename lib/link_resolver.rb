0aed78ad0d0c544a6279481d21b559267547de3b
Albert Bertram <bertrama@umich.edu>
2026-04-23 22:05:54 -0400

    ruby 4 compat.

--- lib/link_resolver.rb
+++ lib/link_resolver.rb
@@ -1,5 +1,26 @@
 require "active_support"
 require "titlecase"
+require "cgi"
+
+unless CGI.respond_to?(:parse)
+  class CGI
+    # File lib/cgi/core.rb, line 393
+    def self.parse(query)
+      params = {}
+      query.split(/[&;]/).each do |pairs|
+        key, value = pairs.split('=',2).collect{|v| CGI.unescape(v) }
+
+        next unless key
+
+        params[key] ||= []
+        params[key].push(value) if value
+      end
+
+      params.default=[].freeze
+      params
+    end
+  end
+end
 
 require_relative "link_resolver/preprocess_query_string"
 require_relative "link_resolver/proxy"
